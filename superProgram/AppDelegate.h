e75d0b47f3a085822bf00f4cc460a86ee26e8b20
zhumengle126@126.com <zhumengle126@126.com>
2014-10-10 20:28:37 +0800

    init one

---
+++ superProgram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  superProgram
+//
+//  Created by 朱孟乐 on 14-10-10.
+//  Copyright (c) 2014年 朱孟乐. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ superProgram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  superProgram
+//
+//  Created by 朱孟乐 on 14-10-10.
+//  Copyright (c) 2014年 朱孟乐. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

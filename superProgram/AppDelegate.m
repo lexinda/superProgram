d80af439bb49d2ec3739de4bf1a00b619250f00a
zhumengle126@126.com <zhumengle126@126.com>
2014-10-10 20:40:50 +0800

    删除xib，storyboard

--- superProgram/AppDelegate.m
+++ superProgram/AppDelegate.m
@@ -14,9 +14,12 @@
 
 @implementation AppDelegate
 
-
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    // Override point for customization after application launch.
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
     return YES;
 }
 
